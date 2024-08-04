f7a61cabf21e5e7d93395c0e64063dcd45730a3c
waikoo <beroer@protonmail.com>
2024-08-05 00:52:13 +0300

    Add: animated gradient bg

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="">rooooooot</div>
+      {/* <div className="">rooooooot</div> */}
 
       <Outlet />
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,24 @@ import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: () => (
-    <div className="justify-center gap-10 bg-gray-500 p-3 flex">
-      <Link to="/evelin" >
-        Evelin
-      </Link>
+    <div className="relative">
+      <div className="justify-center bg-white bg-opacity-10 gap-10 p-3 flex absolute top-0 right-0 left-0">
+        <Link to="/evelin" >
+          Evelin
+        </Link>
 
-      <Link to="/barna" >
-        Barna
-      </Link>
+        <Link to="/barna" >
+          Barna
+        </Link>
+      </div>
+
+      <video
+        className="w-full h-full"
+        autoPlay
+        loop
+        muted
+        playsInline
+        src="../public/animated_gradient.mp4"></video>
     </div>
   )
 })
