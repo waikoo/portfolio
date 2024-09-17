c9334b2965881125b0e9213f0712f35cc12e978b
waikoo <beroer@protonmail.com>
2024-09-17 21:16:42 +0300

    Add: content to /barna

---
+++ src/projects/data.ts
@@ -0,0 +1,19 @@
+export const projects = [{
+  id: 1,
+  name: "Thriftstudio",
+  description: "A multi-page e-commerce responsive website for a local thrift store, with an admin interface.",
+  image: "/images/thriftstudio.gif",
+  tags: ["NEXT.JS", "REACT", "TAILWIND", "TYPESCRIPT", "SUPABASE"],
+  live: "https://monumental-zuccutto-62f1b9.netlify.app",
+  github: "https://github.com/waikoo/thrifty",
+},
+{
+  id: 2,
+  name: "Rock, Paper, Scissors",
+  description: "A simple and interactive game website where users can play against the computer, track their score, and enjoy a fun, minimalist design.",
+  image: "/images/rps.gif",
+  tags: ["HTML", "CSS", "JAVASCRIPT"],
+  live: "https://waikoo.github.io/rock-paper-scissors/",
+  github: "https://github.com/waikoo/rock-paper-scissors",
+}
+]
