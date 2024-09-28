e7df707e7ddc0dff8337593d958be6c811421127
waikoo <beroer@protonmail.com>
2024-09-28 16:32:55 +0200

    Fix: build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel'
+// import vercel from '@astrojs/vercel'
 
 
 // https://astro.build/config
