dc563f29a2b1bce45c61178d140d34386715e79c
waikoo <beroer@protonmail.com>
2024-09-28 16:25:17 +0200

    Add: vercel adapter to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel'
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
