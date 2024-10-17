dfdcc79ce8c69738a86baea9be41cca3e1d69747
waikoo <beroer@protonmail.com>
2024-10-17 04:10:52 +0200

    Add: evelinData & morph BarnaProjectCard to accept evelin-specific logic

---
+++ src/projects/evelinData.ts
@@ -0,0 +1,25 @@
+export const projects = [{
+  id: 1,
+  name: "Thriftstudio",
+  description: "A responsive e-commerce website for a local thrift store, with an admin interface.",
+  src: "/images/thriftstudio_preview.jpg",
+  projectLink: "",
+  color: "#d2d62e"
+},
+{
+  id: 2,
+  name: "Rock, Paper, Scissors",
+  description: "A simple and interactive game website where users can play against the computer, track their score, and enjoy a fun, minimalist design.",
+  src: ["/videos/rps_av1.mp4", "/videos/rps.mp4"],
+  projectLink: "",
+  color: "#71E5A5"
+},
+{
+  id: 4,
+  name: "Format Colors",
+  description: "A Chrome extension that matches unique color names to hsl values serving as a tool for frontendmentor.io challenges. The user can choose between SCSS and CSS variables, that are automatically copied to their clipboard.",
+  src: ["/videos/format_colors_av1.mp4", "/videos/format_colors.mp4"],
+  projectLink: "",
+  color: "#00fe8f"
+}
+]
