068932a41322359aae218dd604583c23a1ef79b6
waikoo <beroer@protonmail.com>
2025-01-18 02:07:35 +0100

    Style: drill color prop into style tag of li for quizzical header nav text

---
+++ src/utils/getHeaderNavLinkShadow.ts
@@ -0,0 +1,3 @@
+export const getHeaderNavLinkShadow = (lowercasedName: 'evelin' | 'barna') => {
+  return lowercasedName === 'evelin' ? "hover:drop-shadow-[0px_0px_3px_#e296f5]" : "hover:drop-shadow-[0px_0px_3px_yellow]";
+};
--- src/utils/getHeaderNavLinkTextColor.ts
+++ src/utils/getHeaderNavLinkTextColor.ts
@@ -5,25 +5,51 @@ export const getHeaderNavLinkTextColor = (
   currentUrl: string
 ) => {
   if (href === "/") {
-    if (isHamburger) {
-      return currentUrl.includes("barna") ? "text-black" : "";
-    }
-    if (currentUrl.includes("thriftstudio")) {
-      return "text-black";
-    }
-    return currentUrl.includes("barna") ? "text-white" : "text-[#e296f5]";
+    return getHomeTextColor(isHamburger, currentUrl);
   }
-  if (currentUrl.includes("thriftstudio")) {
-    return "text-black";
+
+  if (href.includes("thriftstudio") || href.includes('quizzical')) {
+    return getProjectSpecificTextColor(href);
   }
   if (isHamburger) {
     return isCurrentPage ? "text-white" : "text-black";
   }
+
+  if (href.includes('evelin') || href.includes('barna')) {
+    return getEvelinOrBarnaTextColor(href, isCurrentPage);
+  }
+};
+
+const getHomeTextColor = (isHamburger: boolean, currentUrl: string) => {
+  if (isHamburger) {
+    return currentUrl.includes("barna") ? "text-black" : "";
+  }
+  if (currentUrl.includes("thriftstudio")) {
+    return "text-black";
+  }
+  if (currentUrl.includes("quizzical")) {
+    return "text-[#EBD302]";
+  }
+  return currentUrl.includes("barna") ? "text-white" : "text-[#e296f5]";
+};
+
+const getEvelinOrBarnaTextColor = (href: string, isCurrentPage: boolean) => {
   if (href.includes("evelin")) {
+    if (href.includes('quizzical')) {
+      return "text-[#EBD302]"
+    }
     return isCurrentPage ? "text-[#efd6f5]" : "text-[#e296f5]";
   }
   if (href.includes("barna")) {
     return isCurrentPage ? "text-[#D9D62D]" : "text-white";
   }
-};
+}
 
+const getProjectSpecificTextColor = (href: string) => {
+  if (href.includes("thriftstudio")) {
+    return "text-black";
+  }
+  if (href.includes("quizzical")) {
+    return "text-[#EBD302]";
+  }
+}
