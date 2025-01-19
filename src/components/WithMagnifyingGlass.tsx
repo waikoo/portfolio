73c2b6ef621f95d15ebc12bee796afa96995e351
waikoo <beroer@protonmail.com>
2025-01-19 23:55:14 +0100

    Add: react to WithMagnifyingGlass component

---
+++ src/components/WithMagnifyingGlass.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import MagnifyingGlass from "./MagnifyingGlass.tsx";
+
+interface Props {
+  children: React.ReactNode
+  invert: boolean
+}
+
+export default function WithMagnifyingGlass({ children, invert }: Props) {
+  const [text, setText] = useState<'Click' | 'Tap' | null>(null)
+  const invertFlex = invert ? 'flex-row-reverse' : ''
+
+  useEffect(() => {
+    const TAILWIND_XL = 1280
+    const setMyText = () => {
+      window.matchMedia(`(min-width: ${TAILWIND_XL}px)`).matches ? setText('Click') : setText('Tap')
+    }
+    setMyText()
+    window.addEventListener('resize', setMyText)
+
+    return () => setMyText()
+  })
+
+  return (
+    <figure>
+      {children}
+      <figcaption
+        className={`pt-[20px] text-[13px] text-[#8d8d8d] sm:text-[14px] flex ${invertFlex} gap-2  items-center justify-center`}
+      >
+        <MagnifyingGlass />
+        <span>{text} to enlarge</span>
+      </figcaption>
+    </figure>
+
+  )
+}
