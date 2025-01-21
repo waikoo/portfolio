e171a2187ede047f987afc40ba592af176423478
waikoo <beroer@protonmail.com>
2025-01-21 19:18:03 +0100

    Change: order of projects, display them in reverse order, new on top

--- src/projects/data.ts
+++ src/projects/data.ts
@@ -1,51 +1,52 @@
-export const projects = [{
-  id: 0,
-  name: "Quizzical",
-  description: "Quizzical is a trivia game designed to entertain and challenge users with a wide variety of engaging questions across diverse topics.",
-  src: "/images/quizzical.avif",
-  tags: ["SVELTE", "TAILWIND", "TYPESCRIPT"],
-  live: "https://quizzical-wine.vercel.app",
-  github: "https://github.com/waikoo/quizzical",
-  color: "#FF90C2"
-},
-{
-  id: 1,
-  name: "Thriftstudio",
-  description: "A responsive e-commerce website for a local thrift store, with an admin interface.",
-  src: "/images/thriftstudio_preview.jpg",
-  tags: ["NEXT.JS", "REACT", "TAILWIND", "TYPESCRIPT", "SUPABASE"],
-  live: "https://monumental-zuccutto-62f1b9.netlify.app",
-  github: "https://github.com/waikoo/thrifty",
-  color: "#d2d62e"
-},
-{
-  id: 2,
-  name: "Rock, Paper, Scissors",
-  description: "A simple and interactive game website where users can play against the computer, track their score, and enjoy a fun, minimalist design.",
-  src: ["/videos/rps_av1.mp4", "/videos/rps.mp4"],
-  tags: ["HTML", "SCSS", "TYPESCRIPT"],
-  live: "https://waikoo.github.io/rock-paper-scissors/",
-  github: "https://github.com/waikoo/rock-paper-scissors",
-  color: "#71E5A5"
-},
-{
-  id: 3,
-  name: "Guess My Number",
-  description: "A colorful Linux CLI game where users can guess a random number within the constraints of their choosing.",
-  src: ["/videos/guess-my-number_av1.mp4", "/videos/guess-my-number.mp4"],
-  tags: ["NODE", "TYPESCRIPT"],
-  live: "",
-  github: "https://github.com/waikoo/guess-my-number",
-  color: "#B99EF3"
-},
-{
-  id: 4,
-  name: "Format Colors",
-  description: "A Chrome extension that matches unique color names to hsl values serving as a tool for frontendmentor.io challenges. The user can choose between SCSS and CSS variables, that are automatically copied to their clipboard.",
-  src: ["/videos/format_colors_av1.mp4", "/videos/format_colors.mp4"],
-  tags: ["HTML", "CSS", "JAVASCRIPT"],
-  live: "",
-  github: "https://github.com/waikoo/FormatColors",
-  color: "#00fe8f"
-}
+export const projects = [
+  {
+    id: 0,
+    name: "Format Colors",
+    description: "A Chrome extension that matches unique color names to hsl values serving as a tool for frontendmentor.io challenges. The user can choose between SCSS and CSS variables, that are automatically copied to their clipboard.",
+    src: ["/videos/format_colors_av1.mp4", "/videos/format_colors.mp4"],
+    tags: ["HTML", "CSS", "JAVASCRIPT"],
+    live: "",
+    github: "https://github.com/waikoo/FormatColors",
+    color: "#00fe8f"
+  },
+  {
+    id: 1,
+    name: "Rock, Paper, Scissors",
+    description: "A simple and interactive game website where users can play against the computer, track their score, and enjoy a fun, minimalist design.",
+    src: ["/videos/rps_av1.mp4", "/videos/rps.mp4"],
+    tags: ["HTML", "SCSS", "TYPESCRIPT"],
+    live: "https://waikoo.github.io/rock-paper-scissors/",
+    github: "https://github.com/waikoo/rock-paper-scissors",
+    color: "#71E5A5"
+  },
+  {
+    id: 2,
+    name: "Guess My Number",
+    description: "A colorful Linux CLI game where users can guess a random number within the constraints of their choosing.",
+    src: ["/videos/guess-my-number_av1.mp4", "/videos/guess-my-number.mp4"],
+    tags: ["NODE", "TYPESCRIPT"],
+    live: "",
+    github: "https://github.com/waikoo/guess-my-number",
+    color: "#B99EF3"
+  },
+  {
+    id: 3,
+    name: "Thriftstudio",
+    description: "A responsive e-commerce website for a local thrift store, with an admin interface.",
+    src: "/images/thriftstudio_preview.jpg",
+    tags: ["NEXT.JS", "REACT", "TAILWIND", "TYPESCRIPT", "SUPABASE"],
+    live: "https://monumental-zuccutto-62f1b9.netlify.app",
+    github: "https://github.com/waikoo/thrifty",
+    color: "#d2d62e"
+  },
+  {
+    id: 4,
+    name: "Quizzical",
+    description: "Quizzical is a trivia game designed to entertain and challenge users with a wide variety of engaging questions across diverse topics.",
+    src: "/images/quizzical.avif",
+    tags: ["SVELTE", "TAILWIND", "TYPESCRIPT"],
+    live: "https://quizzical-wine.vercel.app",
+    github: "https://github.com/waikoo/quizzical",
+    color: "#FF90C2"
+  },
 ]
--- src/projects/evelinData.ts
+++ src/projects/evelinData.ts
@@ -1,25 +1,27 @@
-export const projects = [{
-  id: 0,
-  name: "Quizzical",
-  description: "Quizzical is a trivia game designed to entertain and challenge users with a wide variety of engaging questions across diverse topics.",
-  src: "/images/quizzical.avif",
-  projectLink: "/evelin/projects/quizzical",
-  color: "#d2d62e"
-},
-{
-  id: 1,
-  name: "Thriftstudio",
-  description: "A responsive e-commerce website for a local thrift store, with an admin interface.",
-  src: "/images/thriftstudio_preview.jpg",
-  projectLink: "/evelin/projects/thriftstudio",
-  color: "#d2d62e"
-},
-{
-  id: 2,
-  name: "Rock, Paper, Scissors",
-  description: "A simple and interactive game website where users can play against the computer, track their score, and enjoy a fun, minimalist design.",
-  src: ["/videos/rps_av1.mp4", "/videos/rps.mp4"],
-  projectLink: "",
-  color: "#d2d62e"
-},
+const color = "#d2d62e"
+export const projects = [
+  {
+    id: 0,
+    name: "Rock, Paper, Scissors",
+    description: "A simple and interactive game website where users can play against the computer, track their score, and enjoy a fun, minimalist design.",
+    src: ["/videos/rps_av1.mp4", "/videos/rps.mp4"],
+    projectLink: "",
+    color: color
+  },
+  {
+    id: 1,
+    name: "Thriftstudio",
+    description: "A responsive e-commerce website for a local thrift store, with an admin interface.",
+    src: "/images/thriftstudio_preview.jpg",
+    projectLink: "/evelin/projects/thriftstudio",
+    color: color
+  },
+  {
+    id: 2,
+    name: "Quizzical",
+    description: "Quizzical is a trivia game designed to entertain and challenge users with a wide variety of engaging questions across diverse topics.",
+    src: "/images/quizzical.avif",
+    projectLink: "/evelin/projects/quizzical",
+    color: color
+  },
 ]
