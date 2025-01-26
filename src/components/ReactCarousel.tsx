969e2226ab32989a180803457839b9ed832e71bb
waikoo <beroer@protonmail.com>
2025-01-26 01:13:56 +0100

    Style: update final design styles on larger viewports too

--- src/components/ReactCarousel.tsx
+++ src/components/ReactCarousel.tsx
@@ -10,14 +10,14 @@ export default function ReactCarousel({ items, children, handleClick, index }: P
   return (
     <>
       <ul
-        className="flex flex-row gap-[8px] sm:justify-between pr-2 font-semibold text-[10px] overflow-scroll items-center border-[2px] sm:border-[3px] rounded-full p-1 border-[#E1CA02] h-max-content uppercase scrollbar-0"
+        className="flex flex-row gap-[8px] sm:justify-between pr-2 font-semibold text-[10px] sm:text-[12px] xl:text-[16px] overflow-scroll items-center border-[2px] sm:border-[3px] rounded-full p-1 border-[#E1CA02] h-max-content uppercase scrollbar-0"
         style={{ scrollbarWidth: 'none' }}
       >
         {
           items.map(({ name }, i) => (
             <li
               key={`quizzical-state-carousel-${i}`}
-              className="cursor-pointer state whitespace-nowrap h-[46px] sm:h-[52px] xl:h-auto grid place-items-center rounded-full px-[10px]"
+              className="cursor-pointer state whitespace-nowrap h-[46px] sm:h-[27px] xl:h-[41px] grid place-items-center rounded-full px-[10px]"
               data-img={`${items[i].imgName}`}
               style={{
                 backgroundColor: index == i ? items[i].color : '',
