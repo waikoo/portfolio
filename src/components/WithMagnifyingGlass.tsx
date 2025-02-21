5bab768cfdc5a09dfa1c3b86367179a5139a9de5
waikoo <beroer@protonmail.com>
2025-02-21 23:33:27 +0100

    Add: components section to /thriftstudio

--- src/components/WithMagnifyingGlass.tsx
+++ src/components/WithMagnifyingGlass.tsx
@@ -6,10 +6,11 @@ interface Props {
   children: React.ReactNode
   invert: boolean
   className?: string
+  hideOnXl?: boolean
 }
 const TAILWIND_XL = 1280
 
-export default function WithMagnifyingGlass({ children, invert, className }: Props) {
+export default function WithMagnifyingGlass({ children, invert, className, hideOnXl }: Props) {
   const [text, setText] = useState<'Click' | 'Tap' | null>(null)
   const invertFlex = invert ? 'flex-row-reverse' : ''
   const matches = useMatchMedia(TAILWIND_XL)
@@ -24,7 +25,7 @@ export default function WithMagnifyingGlass({ children, invert, className }: Pro
         {children}
       </div>
       <figcaption
-        className={`text-[13px] text-[#8d8d8d] sm:text-[16px] flex ${invertFlex} gap-2  items-center justify-center`}
+        className={`text-[13px] text-[#8d8d8d] sm:text-[16px] flex ${invertFlex} gap-2  items-center justify-center ${hideOnXl ? 'xl:hidden' : ''}`}
       >
         <MagnifyingGlass />
         <span className="text-[#8d8d8d]">{text} to enlarge</span>
