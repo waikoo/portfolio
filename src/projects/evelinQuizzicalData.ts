05186691bdfa21a536ad319e2592804cf0f421a3
waikoo <beroer@protonmail.com>
2025-02-23 15:11:45 +0100

    Add: alt text

--- src/projects/evelinQuizzicalData.ts
+++ src/projects/evelinQuizzicalData.ts
@@ -1,10 +1,11 @@
 export default [
-  { name: "Home Screen", imgName: "quizzical_home", color: "#4B3757" },
+  { name: "Home Screen", imgName: "quizzical_home", color: "#4B3757", alt: "home screen of the quizzical game" },
   {
     name: "Settings Screen",
     imgName: "quizzical_settings",
     color: "#415737",
+    alt: "settings screen of the quizzical game"
   },
-  { name: "Game Screen", imgName: "quizzical_game", color: "#662929" },
-  { name: "End Screen", imgName: "quizzical_end", color: "#354057" },
+  { name: "Game Screen", imgName: "quizzical_game", color: "#662929", alt: "game screen of the quizzical game" },
+  { name: "End Screen", imgName: "quizzical_end", color: "#354057", alt: "end screen of the quizzical game" },
 ]
