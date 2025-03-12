1d71a197c9c712201d36631d63f8d31dd778f593
waikoo <beroer@protonmail.com>
2025-03-12 22:37:58 +0100

    Style: design review on mobile

--- src/components/ReactCarousel.tsx
+++ src/components/ReactCarousel.tsx
@@ -20,10 +20,10 @@ export default function ReactCarousel({ items, children, handleClick, index, isQ
   const quizzicalStyles = isQuizzical ? "flex w-[92%] mx-auto border-[#E1CA02]" : ""
   const thriftstudioStyles = isCompetitiveAnalysis || isAdminInterface || isHome || isFinalAccount || isProductsFilters || isCheckout ? "flex border-[#D2D62E]" : ""
   const competitiveAnalysisStyles = isAdminInterface ? "flex w-fit mt-[30px] sm:mt-[40px]" : ""
-  const isHomeStyles = isHome ? "text-[10px] sm:text-[14px] sm:h-[50px] xl:text-[16px] sm:w-[440px] xl:w-[91%] justify-between" : "text-[12px] gap-[8px] sm:justify-between "
-  const isFinalAccountStyles = isFinalAccount ? "!text-[10px] sm:!text-[14px] xl:!text-[16px] !gap-0 xl:w-[425px] justify-between" : ""
-  const productsFiltersStyles = isProductsFilters ? "!text-[10px] sm:!text-[14px]" : ""
-  const checkoutStyles = isCheckout ? "!text-[10px] sm:!text-[14px] sm:w-[304px] justify-between" : ""
+  const isHomeStyles = isHome ? "text-[12px] sm:text-[14px] sm:h-[50px] xl:text-[16px] sm:w-[440px] xl:w-[91%] justify-between" : "text-[12px] gap-[8px] sm:justify-between "
+  const isFinalAccountStyles = isFinalAccount ? "!text-[12px] sm:!text-[14px] xl:!text-[16px] !gap-0 xl:w-[425px] justify-between" : ""
+  const productsFiltersStyles = isProductsFilters ? "!text-[12px] sm:!text-[14px] justify-between w-[340px] w-full" : ""
+  const checkoutStyles = isCheckout ? "!text-[12px] sm:!text-[14px] sm:w-[304px] justify-between w-[190px] sm:w-[320px]" : ""
 
   return (
     <>
@@ -35,7 +35,7 @@ export default function ReactCarousel({ items, children, handleClick, index, isQ
           items.map(({ name }, i) => {
             return (
               <li
-                className={`cursor-pointer state whitespace-nowrap ${isHome ? 'w-[71px] h-[35px] sm:w-[125px] sm:h-[40px]' : 'sm:h-[27px] h-[46px] '} ${isFinalAccount ? 'w-[90px] sm:w-[125px] sm:h-[40px]' : ""} ${isProductsFilters ? 'sm:h-[40px] sm:w-[135px]' : ''} ${isCheckout ? "w-[100px] sm:w-[135px] sm:h-[40px]" : ""} xl:h-[41px] grid place-items-center rounded-full px-[16px]`}
+                className={`cursor-pointer state whitespace-nowrap ${isHome ? 'w-[71px] h-[35px] sm:w-[125px] sm:h-[40px]' : 'sm:h-[27px] h-[35px] '} ${isFinalAccount ? 'w-[90px] sm:w-[125px] sm:h-[40px]' : ""} ${isProductsFilters ? 'h-[35px] sm:h-[40px] sm:w-[135px]' : ''} ${isCheckout ? "h-[35px] w-[100px] sm:w-[135px] sm:h-[40px]" : ""} xl:h-[41px] grid place-items-center rounded-full px-[16px]`}
                 key={`quizzical-state-carousel-${i}`}
                 data-img={`${items[i].imgName}`}
                 style={{
