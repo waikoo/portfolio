3bf9f8bb9fa3940070be645c2e22938df6d691a8
waikoo <beroer@protonmail.com>
2025-03-27 20:58:34 +0100

    Add: contact form to barna/projects, barna/cv, evelin/projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [react()],
+
   redirects: {
     '/barna': '/barna/projects',
     '/evelin': '/evelin/projects'
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
-  clientRouter: import.meta.env.PROD ? true : false
 
+  clientRouter: import.meta.env.PROD ? true : false,
+  adapter: vercel()
 });
